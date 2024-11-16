0601d4f5cf9a11fbd86517510b1d2e4838da12ae
Martosky <imoniaromartins@yahoo.com>
2024-11-16 23:25:02 +0100

    accessing the dom

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const playerScore =document.getElementById("player-score")
+const rockBtn = document.getElementById("rock-btn");
+const paperBtn = document.getElementById("paper-btn");
+const scissorsBtn = document.getElementById("scissors-btn");
+const resultMsg = document.getElementById("result-msg");
+const winnerMsg = document.getElementById("winner-msg");
+const resetBtn = document.getElementById("reset-btn")
