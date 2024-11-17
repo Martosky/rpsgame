f3bf31110bb45174b79aa6077998fe766855795e
Martosky <imoniaromartins@yahoo.com>
2024-11-17 23:51:48 +0100

    added more programme

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ const paperBtn = document.getElementById("paper-btn");
 const scissorsBtn = document.getElementById("scissors-btn");
 const resultMsg = document.getElementById("result-msg");
 const winnerMsg = document.getElementById("winner-msg");
-const resetBtn = document.getElementById("reset-btn")
+const resetBtn = document.getElementById("reset-btn");
+
+function getRandomComputerResult(){
+    let Options = ["Rock","Paper","Scissors"];
+    let randomCheck = Math.floor(Math.random() * Option.length)
+    return Options[randomCheck]
+}
 
 function checkPlayerChoice(){
     return(
